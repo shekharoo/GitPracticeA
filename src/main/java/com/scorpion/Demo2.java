29947e3bbc977e441f6b769820490308c80060b9
Anand <anandmail2013@gmail.com>
2025-12-14 19:13:36 +0530

    Demo2 changes

--- src/main/java/com/scorpion/Demo2.java
+++ src/main/java/com/scorpion/Demo2.java
@@ -9,5 +9,7 @@ public class Demo2 {
         System.out.println("This is GitHub Demo");
         //Changes by Shekhar
         System.out.println("Rename branch from Shekhar to Develop");
+        //Changes by Shekhar
+        System.out.println("This changes have to merge in master branch..");
     }
 }
