42792de527405c8593e27ca5e6b90f04d364882c
Anand <anandmail2013@gmail.com>
2025-12-14 22:21:01 +0530

    This file to be commited to master.

--- src/main/java/com/scorpion/Demo.java
+++ src/main/java/com/scorpion/Demo.java
@@ -2,9 +2,9 @@ package com.scorpion;
 
 public class Demo {
     public static void main(String[] args) {
-
         System.out.println("Merging to master");
         System.out.println("Merging..");
+        System.out.println("This is file to be commited to master..");
 
     }
 }
