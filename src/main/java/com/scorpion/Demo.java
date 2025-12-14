7c9880107ad22c5eb1a2def5f564b82da36f0961
Anand <anandmail2013@gmail.com>
2025-12-14 21:51:45 +0530

    Master1

--- src/main/java/com/scorpion/Demo.java
+++ src/main/java/com/scorpion/Demo.java
@@ -3,5 +3,6 @@ package com.scorpion;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("This is Demo class..");
+        System.out.println("Merging to master");
     }
 }
