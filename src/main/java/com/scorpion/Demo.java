48d3e83f9715021ef4468075ae85d8994d7cd13f
Anand <anandmail2013@gmail.com>
2025-12-18 16:53:32 +0530

    Conflict

--- src/main/java/com/scorpion/Demo.java
+++ src/main/java/com/scorpion/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("This is Demo class..");
         System.out.println("Master");
+        System.out.println("Updating");
     }
 }
