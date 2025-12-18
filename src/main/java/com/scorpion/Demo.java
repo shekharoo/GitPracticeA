03e07c640624b864af310277a17aa8e20fa9cb4f
Anand <anandmail2013@gmail.com>
2025-12-18 16:52:18 +0530

    updating

--- src/main/java/com/scorpion/Demo.java
+++ src/main/java/com/scorpion/Demo.java
@@ -4,5 +4,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("This is Demo class..");
         System.out.println("Master");
+        //Committing this on 18th Dec
+        System.out.println("Committing on 18th Dec");
     }
 }
